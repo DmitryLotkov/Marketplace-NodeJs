e5dbb09e4f75fc53e825364eb022195e259ae05f
Dmitry Lotkov <dmitry.lotkov@gmail.com>
2025-01-04 21:33:48 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-    let helloMessage = 'Hello W444orld!';
+    let helloMessage = 'Hello World!';
     res.send(helloMessage)
 })
 
